d7700d345114241f0b278befc509a71098ec6f34
Steven Starnes <starnes81@gmail.com>
2017-07-12 18:54:52 -0500

    Added video_list.js
    Added code to video_list.js import/exported VideoList.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDBVE9LSaCcU-K71xCnjn-XrBSqJCWMxvo';
 
 
@@ -26,6 +26,7 @@ constructor(props) {
 		return (
 			<div>
 				<SearchBar />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
