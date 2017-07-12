cd3de1c01f7bd3f47b66ddef4eabf299931d1a1e
Steven Starnes <starnes81@gmail.com>
2017-07-12 18:38:46 -0500

    Update search API
    Update code from const to class, added constructor, and import component.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDBVE9LSaCcU-K71xCnjn-XrBSqJCWMxvo';
 
-// New component to produce html
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+
+
+class App extends Component {
+constructor(props) {
+	super(props);
+
+	this.state = { videos: [] };
+	// start search
+	YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+		//update state
+		this.setState ({ videos });
+		// this.setState ({ videos: videos});
+	});
+}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
  
 // display components html on a page (DOM)
